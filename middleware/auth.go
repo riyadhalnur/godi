fc87eef4de073c1b3511e9d481c55cb0e3793562
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2020-05-07 18:22:55 +0800

    Add comments to middleware functions

--- middleware/auth.go
+++ middleware/auth.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 )
 
+// Authenticate middleware is used to authenticate protected
+// api routes. Mounted to authenticate any route under /api
 func Authenticate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token := r.Header.Get("Authorization")
--- middleware/logging.go
+++ middleware/logging.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+// Logging is a basic logging middleware that logs
+// any incoming request to stdout
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		logger := log.New(os.Stdout, "[INFO]: ", log.LstdFlags)
